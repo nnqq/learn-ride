6b8a0fa61f6461b9a31cf2ea13234b61c221284e
nnqq <43930873+nnqq@users.noreply.github.com>
2022-01-31 12:14:29 +0300

    wip

--- test/main_test.go
+++ test/main_test.go
@@ -1,14 +1,23 @@
 package test
 
 import (
+	"context"
+	"encoding/base64"
 	"github.com/stretchr/testify/assert"
+	"github.com/wavesplatform/gowaves/pkg/client"
 	"github.com/wavesplatform/gowaves/pkg/ride"
 	"io"
+	"net/http"
 	"os"
+	"strings"
 	"testing"
+	"time"
 )
 
 func TestCompile(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
 	f, err := os.Open("../ride/main.ride")
 	if !assert.NoError(t, err, "os.Open") {
 		t.FailNow()
@@ -19,9 +28,20 @@ func TestCompile(t *testing.T) {
 		t.FailNow()
 	}
 
-	//client.NewUtils().ScriptCompile()
+	sc, _, err := client.NewUtils(client.Options{
+		BaseUrl: "https://nodes.wavesnodes.com",
+		Client:  &http.Client{Timeout: 3 * time.Second},
+	}).ScriptCompile(ctx, string(body))
+	if !assert.NoError(t, err, "client.NewUtils.ScriptCompile") {
+		t.FailNow()
+	}
 
-	tree, err := ride.Parse(body)
+	src, err := base64.StdEncoding.DecodeString(strings.TrimPrefix(sc.Script, "base64:"))
+	if !assert.NoError(t, err, "base64.StdEncoding.DecodeString") {
+		t.FailNow()
+	}
+
+	tree, err := ride.Parse(src)
 	if !assert.NoError(t, err, "ride.Parse") {
 		t.FailNow()
 	}
