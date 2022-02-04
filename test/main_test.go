f259a8534d703f3962b161801a24b749d387cea2
nnqq <43930873+nnqq@users.noreply.github.com>
2022-02-04 12:02:17 +0300

    wip

--- test/main_test.go
+++ test/main_test.go
@@ -3,6 +3,7 @@ package test
 import (
 	"context"
 	"encoding/base64"
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"github.com/wavesplatform/gowaves/pkg/client"
 	"github.com/wavesplatform/gowaves/pkg/ride"
@@ -14,35 +15,44 @@ import (
 	"time"
 )
 
-func TestCompile(t *testing.T) {
+func getTree() (*ride.Tree, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	f, err := os.Open("../ride/main.ride")
-	if !assert.NoError(t, err, "os.Open") {
-		t.FailNow()
+	if err != nil {
+		return nil, fmt.Errorf("os.Open: %w", err)
 	}
 
 	body, err := io.ReadAll(f)
-	if !assert.NoError(t, err, "io.ReadAll") {
-		t.FailNow()
+	if err != nil {
+		return nil, fmt.Errorf("io.ReadAll: %w", err)
 	}
 
 	sc, _, err := client.NewUtils(client.Options{
 		BaseUrl: "https://nodes.wavesnodes.com",
 		Client:  &http.Client{Timeout: 3 * time.Second},
 	}).ScriptCompile(ctx, string(body))
-	if !assert.NoError(t, err, "client.NewUtils.ScriptCompile") {
-		t.FailNow()
+	if err != nil {
+		return nil, fmt.Errorf("client.NewUtils.ScriptCompile: %w", err)
 	}
 
 	src, err := base64.StdEncoding.DecodeString(strings.TrimPrefix(sc.Script, "base64:"))
-	if !assert.NoError(t, err, "base64.StdEncoding.DecodeString") {
-		t.FailNow()
+	if err != nil {
+		return nil, fmt.Errorf("base64.StdEncoding.DecodeString: %w", err)
 	}
 
 	tree, err := ride.Parse(src)
-	if !assert.NoError(t, err, "ride.Parse") {
+	if err != nil {
+		return nil, fmt.Errorf("ride.Parse: %w", err)
+	}
+
+	return tree, nil
+}
+
+func TestCompile(t *testing.T) {
+	tree, err := getTree()
+	if !assert.NoError(t, err, "getTree") {
 		t.FailNow()
 	}
 
